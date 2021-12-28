6471435829f1a276d10b1d2b6ff5f202b665a41a
cechava <cesar@cesars-mbp-2.attlocal.net>
2021-12-28 16:32:14 -0500

    updated trial info fetched; livescript polished

--- getTrialInfo.m
+++ getTrialInfo.m
@@ -1,14 +1,16 @@
 function info = getTrialInfo(nwb_file, trial)
 
-choiceTypes = {'Left','No-Go','Right'};
-feedbackTypes = {'Punishment','Reward'};
+choiceTypes = {'Right','No-Go','Left'};
+feedbackTypes = {'Punishment','','Reward'};
 info = struct();
 trial_start_time = nwb_file.intervals_trials.start_time.data(trial);
 info.stim_contrast_left = nwb_file.intervals_trials.vectordata.get('visual_stimulus_left_contrast').data(trial);
 info.stim_contrast_right = nwb_file.intervals_trials.vectordata.get('visual_stimulus_right_contrast').data(trial);
+go_time = nwb_file.intervals_trials.vectordata.get('go_cue').data(trial) - trial_start_time;
 choice = nwb_file.intervals_trials.vectordata.get('response_choice').data(trial);
 info.response_choice = choiceTypes{choice+2};
 info.response_time = nwb_file.intervals_trials.vectordata.get('response_time').data(trial) - trial_start_time;
+info.reaction_time = info.response_time - go_time;
 feedback = nwb_file.intervals_trials.vectordata.get('feedback_type').data(trial);
-info.feedback_type = feedbackTypes{feedback};
+info.feedback_type = feedbackTypes{feedback+2};
 end
