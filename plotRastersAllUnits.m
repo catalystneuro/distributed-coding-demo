d7c5f1b5d062cac9d5180b429cb19c6b609f4e80
cechava <cesar@cesars-mbp-2.attlocal.net>
2021-12-28 12:11:55 -0500

    color-coding of area

--- plotRastersAllUnits.m
+++ plotRastersAllUnits.m
@@ -11,35 +11,40 @@ tone_time = nwb_file.intervals_trials.vectordata.get('go_cue').data(trial) - tri
 % reward
 feedback_time = nwb_file.intervals_trials.vectordata.get('feedback_time').data(trial) - trial_start_time;
 
-probe_list = keys(nwb_file.general_extracellular_ephys.map);
-nprobes = length(probe_list);
-
-% exclude MUA and noise (as in Steinmetz et al 2019 paper)
-annot = nwb_file.units.vectordata.get('phy_annotations').data(:);
-valid_units = find(annot>=2);
-
-% getting number of units
-num_units = length(valid_units);
-
+% time details for raster plots
 before_time = 0;
 after_time = 1 +feedback_time;
 
-%get unit depth (0 = deepest, 3820 = shallowest; scale um)
-depths_all = nwb_file.units.vectordata.get('cluster_depths').data(:);
+%%% Electrode details
+probe_list = keys(nwb_file.general_extracellular_ephys.map);
+nprobes = length(probe_list);
+% electrode location
+electrode_area = nwb_file.general_extracellular_ephys_electrodes.vectordata.get('location').data(:);
 
+%%% All unit/cluster details
+% get cluster annotations
+annot = nwb_file.units.vectordata.get('phy_annotations').data(:);
+% exclude MUA and noise (as in Steinmetz et al 2019 paper)
+valid_units = find(annot>=2);
+num_units = length(valid_units);
+% recording probe
+probe_all = arrayfun( ...
+    @(x) probePathToIdx(x.path, probe_list), ...
+    nwb_file.units.electrode_group.data ...
+);
+% cluster channel
+channel_all = nwb_file.units.vectordata.get('peak_channel').data(:);
+% cluster depth (0 = deepest, 3820 = shallowest; scale um)
+depth_all = nwb_file.units.vectordata.get('cluster_depths').data(:);
+
+% get details for valid units
+depth_unit = depth_all(valid_units);
+probe_unit = probe_all(valid_units);
+area_unit = electrode_area(channel_all(valid_units));
+
+% get raster data
 raster_data = {};
-depth_unit = zeros(1,num_units);
-probe_prov = zeros(1,num_units);
-
 for n = 1:num_units
-    % get unit probe provenance
-    probe_path = nwb_file.units.electrode_group.data(valid_units(n)).path;
-    slash_idx = find(probe_path=='/');
-    probe = probe_path(slash_idx(end)+1:end);
-    idxC = strfind(probe_list,probe);
-    probe_prov(n) = find(not(cellfun('isempty',idxC)));
-    depth_unit(n) = depths_all(valid_units(n));
-
     % get spike times
     spike_times = util.read_indexed_column(nwb_file.units.spike_times_index, ...
                                            nwb_file.units.spike_times, ...
@@ -49,34 +54,47 @@ for n = 1:num_units
                        spike_times <= trial_start_time + after_time) - ...
                        trial_start_time;
 end
-% make figure
-min_depth = 0;% microns
+
+%%% Make figure
+% list areas
+area_list = unique(area_unit);
+num_areas = length(area_list);
+% palette
+palette = hsv(num_areas);
+palette = rgb2hsv(palette);
+palette(:,2) = 0.6; % reduce saturation
+palette = hsv2rgb(palette);
+% depth details
+min_depth = 0;%microns
 max_depth = 3820;% microns
+
 figure;
 for p = 1:nprobes
-    probe_idxs = find(probe_prov==p);
+    probe_idxs = find(probe_unit==p);
     subplot(nprobes, 1, p)
     hold on
     % make raster plot
     for i = 1:length(probe_idxs)
         row_data = raster_data{probe_idxs(i)};
         row_loc = depth_unit(probe_idxs(i));
+        row_color = palette(strcmp(area_unit(probe_idxs(i)),area_list),:);
         plot(row_data, ones(length(row_data),1) * row_loc, '.', ...
-            'Color','#add8e6')
+            'Color',row_color)
     end
     % set and get axis limit
     xlim([before_time after_time])
     ylim([min_depth max_depth])
-    yl = ylim();
     xl = xlim();
     % mark and label time points
     stim_text_loc = (stim_time-xl(1))/(xl(2)-xl(1));
     resp_text_loc = (response_time-xl(1))/(xl(2)-xl(1));
     tone_text_loc = (tone_time-xl(1))/(xl(2)-xl(1));
-    xline([stim_time response_time tone_time feedback_time])
+    xline([stim_time response_time tone_time])
+    xline(feedback_time,'--') % mark feedback with dashed line to limit clutter
     text(stim_text_loc,1.01,'stim','HorizontalAlignment','center','Units','normalized')
     text(resp_text_loc,1.01,'move','HorizontalAlignment','center','Units','normalized')
     text(tone_text_loc,1.01,'tone','HorizontalAlignment','center','Units','normalized')
+    
     % set yaxis ticks
     set(gca,'yTick', fliplr(3820:-1000:0));
     set(gca,'yTickLabel', fliplr(0:1000:3820));
@@ -84,4 +102,20 @@ for p = 1:nprobes
     title(probe_list(p),'Units', 'normalized','Position',[0.5 1.05 0]) 
     xlabel('Time (seconds)')
     ylabel('Depth (\mum)')
+    
 end
+% hacking together legend
+hold on;
+h = zeros(num_areas, 1);
+for i =1:num_areas
+    h(i) = plot(NaN,NaN,'.','Color',palette(i,:));
+end
+legend(h, area_list,'Location','southoutside','Orientation','Horizontal');
+end
+function idx = probePathToIdx(probe_path, probe_list)
+slash_idx = find(probe_path=='/');
+probe = probe_path(slash_idx(end)+1:end);
+idxC = strfind(probe_list,probe);
+idx = find(not(cellfun('isempty',idxC)));
+end 
+
